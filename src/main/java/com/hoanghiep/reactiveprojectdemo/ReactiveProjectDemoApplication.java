bba2e63daa57af78dee631f8be328b391eada18f
hoanghiep <hoanghiep0901@gmail.com>
2021-10-10 10:13:38 +0700

    init database before start project

--- src/main/java/com/hoanghiep/reactiveprojectdemo/ReactiveProjectDemoApplication.java
+++ src/main/java/com/hoanghiep/reactiveprojectdemo/ReactiveProjectDemoApplication.java
@@ -1,13 +1,27 @@
 package com.hoanghiep.reactiveprojectdemo;
 
+import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 
 @SpringBootApplication
 public class ReactiveProjectDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ReactiveProjectDemoApplication.class, args);
+    }
+
+    @Bean
+    public ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator(new ClassPathResource("schema.sql"));
+        initializer.setDatabasePopulator(populator);
+        return initializer;
     }
 
 }
